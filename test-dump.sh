75875edb3744ae81f21f8db791cf3a4f44c00ae3
0xSheepdog <0xSheepdog@gmail.com>
2019-11-12 15:50:43 -0700

    initial commit

---
+++ test-dump.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+my_host="$(hostname -s)"
+my_date="$(date -I)"
+my_vol="${1}"
+my_dump_lvl="0"
+my_dumpfile="${my_vol}.xfsdump"
+
+touch "/dump/${dumpfile}"
+xfsdump -F -l "${my_dump_lvl}" -f "/dump/${dumpfile}" "${my_vol}"
