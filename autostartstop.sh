5cfc70153c15205bd79557af54c873e6f7aa7507
Jeff Pettorino <0xSheepdog@users.noreply.github.com>
2021-05-28 11:11:37 -0600

    Update autostartstop.sh
    Not complete

--- autostartstop.sh
+++ autostartstop.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+#!/bin/bash
+## Poor Mans CDP analyzer
+## v 0.0.1
+## originally conceived and written by M### B#######
+## updated by Jeff Pettorino
+##
+WSVMS="rh7wks01 rh7wks02 rh7wks03 rh7wks04"
+SECVMS="ossec nessus"
+IPAVMS="ipa01 ipa02"
+LOGVMS="llogagg"
+VMGROUPS="WSVMS SECVMS IPAVMS LOGVMS"
 
-WSVMS=(rh7wks01 rh7wks02 rh7wks03 rh7wks04)
-SECVMS=(ossec nessus)
-IPAVMS=(ipa01 ipa02)
-LOGVMS=(llogagg)
-VMGROUPS=(WSVMS SECVMS IPAVMS LOGVMS)
-
-for G in $VMGROUPS
+for G in VMGROUPS
 do
-  for VM in $WSVMS
+  for VM in WSVMS
    do
     echo "Shutting down ${VM}..."
     virsh shutdown $VM
