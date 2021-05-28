e0035d5a9161b19f12a2cbc96538ca1ecb8fc1ee
Jeff Pettorino <0xSheepdog@users.noreply.github.com>
2021-05-28 11:05:35 -0600

    Update autostartstop.sh

--- autostartstop.sh
+++ autostartstop.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+IPAVMS=(ipa01 ipa02)
+LOGVMS=(llogagg)
+SECVMS=(ossec nessus)
+WSVMS=(rh7wks01 rh7wks02 rh7wks03 rh7wks04)
+
+VMGROUPS=(WSVMS SECVMS IPAVMS LOGVMS)
+
+for G in $VMGROUPS
+do
+  for VM in $WSVMS
+   do
+    echo "Shutting down ${VM}..."
+    virsh shutdown $VM
+  done
+  
+  echo "Pausing for a few seconds..."
+  sleep 15
+done
